c7d5f1d70508b9fd0b5b97fc6ebc67bd53d39088
cchiancone <cchiancone@outlook.com>
2017-11-18 08:40:39 -0600

    First

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Christopher Chiancone on 11/18/17.
+//  Copyright © 2017 Abrisys, Inc. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
